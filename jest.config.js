e35eea9874952714e3455cca690f4f0669b3eaa8
Utwo <mihai.legat@gmail.com>
2018-07-02 16:14:45 +0300

    Add one jest example test

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "transform": {
+    "^.+\\.ts?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "collectCoverage": false,
+  "testEnvironment": "node",
+  "collectCoverageFrom": [
+    "src/**/*",
+  ]
+}
