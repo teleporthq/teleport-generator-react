54e4b7cbce6ac444d4e1f8d021636edfd33a9879
Cristian Moldovan <crissmoldovan@gmail.com>
2018-09-05 08:13:17 +0300

    switched to new generator architecture

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
-import { Generator, FileSet } from '@teleporthq/teleport-lib-js'
-import ReactComponentGenerator from './generators/component'
-import ReactProjectGenerator from './generators/project'
+import { Generator } from '@teleporthq/teleport-lib-js'
+
+import StyledJSXReactComponentRenderer from './styled-jsx'
+import ReactJSSReactComponentRenderer from './react-jss'
+import CSSModulesReactComponentRenderer from './css-modules'
 
 export default class TeleportGeneratorReact extends Generator {
-  // @todo: can we avoid redeclaring componentGenerator and projectGenerator since they exist on Generator?
-  public componentGenerator: ReactComponentGenerator
-  public projectGenerator: ReactProjectGenerator
-
   constructor() {
-    super('react-generator', 'react')
+    const componentRenderers = {
+      'css-modules': new CSSModulesReactComponentRenderer(),
+      default: new ReactJSSReactComponentRenderer(),
+      'react-jss': new ReactJSSReactComponentRenderer(),
+      'styled-jsx': new StyledJSXReactComponentRenderer(),
+    }
 
-    this.componentGenerator = new ReactComponentGenerator(this)
-    this.projectGenerator = new ReactProjectGenerator(this, this.componentGenerator)
-  }
-
-  public generateComponent<T, U>(component: T, options: U): FileSet {
-    return this.componentGenerator.generate(component, options)
-  }
-
-  public generateProject(component: any, options: any): FileSet {
-    return this.projectGenerator.generate(component, options)
+    super('react-generator', 'react', componentRenderers)
   }
 }
